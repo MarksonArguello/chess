cadd95b033cf61b2431d9aea4fe843ed2b47defc
Markson Arguello <marksonarguello@gmail.com>
2021-05-05 13:47:55 -0300

    Implementando movimentos possiveis do rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,26 @@ public class King extends ChessPiece{
 		return "K";
 	}
 
+	private boolean canMove(Position position) {
+		return (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position));
+	}
+
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		int row = this.position.getRow();
+		int column = this.position.getColumn();
+		Position position = new Position(row, column);
+
+		for (int i = row - 1; i <= row + 1; i++) {
+			for (int j = column - 1; j <= column + 1; j++) {
+				position.setValues(i, j);
+				if (i == row && j == column) continue;
+				if (getBoard().positionExists(position) && canMove(position)) {
+					mat[i][j] = true;
+				}
+			}
+		}
 
 		return mat;
 	}
